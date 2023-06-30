f8523df65adbc7e517e90b63a0b6dac47b72d201
k12onos <ujjwal2508@protonmail.com>
2023-07-01 03:45:11 +0530

    Made minor changes (mainly changed package name)

---
+++ android/app/src/main/kotlin/app/k12onos/medimate/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.k12onos.medimate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/medimate/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.medimate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
